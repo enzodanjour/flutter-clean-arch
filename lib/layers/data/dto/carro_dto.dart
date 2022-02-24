878d0cc8c86e6afbb9603541e34b74c4363733be
Enzo D <enzo.danjour@gmail.com>
2022-02-23 22:26:55 -0300

    add carro_dto

---
+++ lib/layers/data/dto/carro_dto.dart
@@ -0,0 +1,36 @@
+// faz conversoes de tipos e transferencias de dados
+// entidade nao deve ser modelada de acordo com a api
+// deve ser modelada com a regra de negocios
+import 'package:arq/layers/domain/entities/carro_entity.dart';
+
+class CarroDto extends CarroEntity {
+  String placa;
+  int quantidadeDePortas;
+  double valorFinal;
+
+  CarroDto({
+    required this.placa,
+    required this.quantidadeDePortas,
+    required this.valorFinal,
+  }) : super(
+          placa: placa,
+          qtdPortas: quantidadeDePortas,
+          valor: valorFinal,
+        );
+
+  Map toMap() {
+    return {
+      'placa': placa,
+      'quantidadeDePortas': quantidadeDePortas,
+      'valor': valorFinal,
+    };
+  }
+
+  CarroDto fromMap(Map map) {
+    return CarroDto(
+      placa: map['placa'],
+      quantidadeDePortas: map['quantidadeDePortas'],
+      valorFinal: map['valorFinal'],
+    );
+  }
+}
