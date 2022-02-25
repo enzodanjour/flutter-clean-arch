5e195ad377d9ca9122c998f1e48b714252306840
Enzo D <enzo.danjour@gmail.com>
2022-02-24 23:04:19 -0300

    add instances of datasource

--- test/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_impl_test.dart
+++ test/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_impl_test.dart
@@ -1,25 +1,20 @@
+import 'package:arq/layers/data/datasources/get_carros_por_cor_datasource.dart';
+import 'package:arq/layers/data/datasources/local/get_carros_por_cor_local_datasource_imp.dart';
+import 'package:arq/layers/data/repositories/get_carro_por_cor_repository_imp.dart';
 import 'package:arq/layers/domain/entities/carro_entity.dart';
-import 'package:arq/layers/domain/repositories/get_carros_por_cor_repository.dart';
 import 'package:arq/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase.dart';
 import 'package:arq/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_impl.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-class GetCarrosPorCorRepositoryImp implements GetCarrosPorCorRepository {
-  @override
-  CarroEntity call(String cor) {
-    if (cor.contains('vermelho')) {
-      return CarroEntity(placa: 'abc1234', qtdPortas: 4, valor: 5000.00);
-    }
-    return CarroEntity(placa: 'qwe7897', qtdPortas: 2, valor: 2000.00);
-  }
-}
+
 
 void main() {
   test(
     'Deve retornar uma instancia de carro quando passado qualquer cor',
     () {
+      GetCarrosPorCorDatasource datasource = GetCarrosPorCorLocalDatasourceImp();
       GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImpl(
-        GetCarrosPorCorRepositoryImp(),
+        GetCarrosPorCorRepositoryImp(datasource),
       );
 
       var result = useCase('azul');
@@ -31,8 +26,9 @@ void main() {
   test(
     'Deve retornar um carro de 4 portas quando vermelho',
     () {
+      GetCarrosPorCorDatasource datasource = GetCarrosPorCorLocalDatasourceImp();
       GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImpl(
-        GetCarrosPorCorRepositoryImp(),
+        GetCarrosPorCorRepositoryImp(datasource),
       );
 
       var result = useCase('vermelho');
@@ -44,8 +40,9 @@ void main() {
   test(
     'Deve retornar um carro de 2 portas quando qualquer cor exceto vermelho',
     () {
+       GetCarrosPorCorDatasource datasource = GetCarrosPorCorLocalDatasourceImp();
       GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImpl(
-        GetCarrosPorCorRepositoryImp(),
+        GetCarrosPorCorRepositoryImp(datasource),
       );
 
       var result = useCase('azul');
