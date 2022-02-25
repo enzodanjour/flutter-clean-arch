eeb474723975546431d71e4c3b39540a9914be61
Enzo D <enzo.danjour@gmail.com>
2022-02-25 00:05:31 -0300

    add init on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
-main(){
-  
+import 'package:arq/core/inject/inject.dart';
+import 'package:arq/layers/presentation/ui/pages/carros_page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  Inject.init();
+  runApp(MaterialApp(
+    home: CarrosPage(),
+  ));
 }
