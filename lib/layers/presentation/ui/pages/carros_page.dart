d0e64d21bdbf3264cc7ce287095b52c33903b7fc
Enzo D <enzo.danjour@gmail.com>
2022-02-25 00:06:35 -0300

    add carros page controller

--- lib/layers/presentation/ui/pages/carros_page.dart
+++ lib/layers/presentation/ui/pages/carros_page.dart
@@ -1,26 +1,11 @@
-import 'package:arq/layers/data/datasources/local/get_carros_por_cor_local_datasource_imp.dart';
-import 'package:arq/layers/data/repositories/get_carro_por_cor_repository_imp.dart';
-import 'package:arq/layers/data/repositories/salvar_carro_favorito_repository_impl.dart';
-import 'package:arq/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_impl.dart';
-import 'package:arq/layers/domain/usecases/salvar_carro_favorito/salvar_carro_favorito_usecase_impl.dart';
 import 'package:arq/layers/presentation/controllers/carro_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class CarrosPage extends StatelessWidget {
   CarrosPage({Key? key}) : super(key: key);
 
-  CarroController controller = CarroController(
-    GetCarrosPorCorUseCaseImpl(
-      GetCarrosPorCorRepositoryImp(
-        GetCarrosPorCorLocalDatasourceImp()
-      )
-    ),
-    SalvarCarroFavoritoUseCaseImpl(
-      SalvarCarroFavoritoRepositoryImpl(
-
-      )
-    )
-  );
+  var _controller = GetIt.instance.get<CarroController>();
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +15,7 @@ class CarrosPage extends StatelessWidget {
       ),
       body: Container(
         padding: EdgeInsets.all(24),
-        child: Text(controller.carro.placa),
+        child: Text(_controller.carro.placa),
       ),
     );
   }
