9e587f26b780e6dfc78f2d1bf7b3909fddcbd99a
Enzo D <enzo.danjour@gmail.com>
2022-02-24 21:59:11 -0300

    add carro impl

--- lib/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_impl.dart
+++ lib/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_impl.dart
@@ -1,15 +1,18 @@
-import 'package:arq/layers/domain/entities/carro_entity.dart';
+
+import '../../entities/carro_entity.dart';
+import '../../repositories/get_carros_por_cor_repository.dart';
 import 'get_carros_por_cor_usecase.dart';
 
 // so importa da camada mais externa
 class GetCarrosPorCorUseCaseImpl implements GetCarrosPorCorUseCase {
 
+  final GetCarrosPorCorRepository _repository;
+
+  GetCarrosPorCorUseCaseImpl(this._repository);
+
   @override
   CarroEntity call(String cor) {
-    if(cor.contains('vermelho')){
-      return CarroEntity(placa: 'abc1234', qtdPortas: 4, valor: 5000.00);
-    }
-    return CarroEntity(placa: 'qwe7897', qtdPortas: 2, valor: 2000.00);
+    return _repository(cor);
   }
 
 }
