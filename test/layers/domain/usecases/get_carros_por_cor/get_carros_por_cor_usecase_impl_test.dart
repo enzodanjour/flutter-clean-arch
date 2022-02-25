d9e474ca352ecb9f6b0e1aa9aab1cf4b8d3e271b
Enzo D <enzo.danjour@gmail.com>
2022-02-24 21:58:43 -0300

    get carros repository impl

--- test/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_impl_test.dart
+++ test/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_impl_test.dart
@@ -1,13 +1,26 @@
 import 'package:arq/layers/domain/entities/carro_entity.dart';
+import 'package:arq/layers/domain/repositories/get_carros_por_cor_repository.dart';
 import 'package:arq/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase.dart';
 import 'package:arq/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_impl.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+class GetCarrosPorCorRepositoryImp implements GetCarrosPorCorRepository {
+  @override
+  CarroEntity call(String cor) {
+    if (cor.contains('vermelho')) {
+      return CarroEntity(placa: 'abc1234', qtdPortas: 4, valor: 5000.00);
+    }
+    return CarroEntity(placa: 'qwe7897', qtdPortas: 2, valor: 2000.00);
+  }
+}
 
 void main() {
   test(
     'Deve retornar uma instancia de carro quando passado qualquer cor',
     () {
-      GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImpl();
+      GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImpl(
+        GetCarrosPorCorRepositoryImp(),
+      );
 
       var result = useCase('azul');
 
@@ -18,7 +31,9 @@ void main() {
   test(
     'Deve retornar um carro de 4 portas quando vermelho',
     () {
-      GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImpl();
+      GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImpl(
+        GetCarrosPorCorRepositoryImp(),
+      );
 
       var result = useCase('vermelho');
 
@@ -29,13 +44,13 @@ void main() {
   test(
     'Deve retornar um carro de 2 portas quando qualquer cor exceto vermelho',
     () {
-      GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImpl();
+      GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImpl(
+        GetCarrosPorCorRepositoryImp(),
+      );
 
       var result = useCase('azul');
 
       expect(result.qtdPortas, 2);
     },
   );
-
-  
 }
